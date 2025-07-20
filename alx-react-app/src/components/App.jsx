7e6547c7d501c72f818090c71efcdabe7dcdd8d8
LawrenceNgomani <ltngomani@gmail.com>
2025-07-20 13:23:23 -0700

    New week updates

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,20 +1,12 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import UserProfile from './components/UserProfile'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-      <WelcomeMessage />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </>
-  )
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
-export default App
+export default App;
